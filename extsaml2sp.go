9993503008e1199670967af90bdfe53af39b7dce
sgonzalez <sgonzalez@atricore.com>
2021-07-12 14:40:46 -0400

    Minor chages to comments

--- extsaml2sp.go
+++ extsaml2sp.go
@@ -18,6 +18,7 @@ func (c *IdbusApiClient) CreateExtSaml2Sp(ida string, sp api.ExternalSaml2Servic
 		return result, err
 	}
 
+	// TODO : si this necessary for ext. SPs ?
 	// initSP(&sp)
 
 	ctx := context.WithValue(context.Background(), api.ContextAccessToken, sc.Authn.AccessToken)

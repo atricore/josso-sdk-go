6809d618c868f4ce1c556b28efd5a60f222a1f05
Sebastian <sgonzalezoyuela@gmail.com>
2022-07-27 09:58:57 -0400

    FIX : Google acctest

--- id_google_test.go
+++ id_google_test.go
@@ -55,7 +55,6 @@ func (s *AccTestSuite) TestAccCliIdpGoogle_crud() {
 
 	// Test Update
 	read.Description = api.PtrString("Updated description")
-	read.ElementId = api.PtrString("12345")
 
 	updated, err := s.client.UpdateIdpGoogle(*appliance.Name, read)
 	if err != nil {

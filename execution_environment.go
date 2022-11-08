0dedfae8308231ca7881e1d4e78c24edc75c0ed0
Sebastian <sgonzalezoyuela@gmail.com>
2022-11-07 19:59:12 -0500

    new exec env config export

--- execution_environment.go
+++ execution_environment.go
@@ -7,6 +7,38 @@ import (
 	api "github.com/atricore/josso-api-go"
 )
 
+func (c *IdbusApiClient) ExportExecEnvCfg(appliance_id string, exec_env_name string) (string, string, error) {
+	c.logger.Debugf("exportExecEnvCfg [%s] id: %s", appliance_id, exec_env_name)
+	sc, err := c.IdbusServerForOperation("DefaultApiService.exportExecEnvCfg") // Also hard-coded in generated client
+	if err != nil {
+		c.logger.Errorf("exportExecEnvCfg. Error %v", err)
+		return "", "", err
+	}
+
+	ctx := context.WithValue(context.Background(), api.ContextAccessToken, sc.Authn.AccessToken)
+	req := c.apiClient.DefaultApi.ExportExecEnvCfg(ctx)
+	req = req.ExportExecEnvCfgReq(api.ExportExecEnvCfgReq{
+		IdaName: &appliance_id,
+		Name:    &exec_env_name,
+	})
+	res, _, err := c.apiClient.DefaultApi.ExportExecEnvCfgExecute(req)
+
+	if err != nil {
+		c.logger.Errorf("exportExecEnvCfg. Error %v", err)
+		return "", "", err
+	}
+
+	if res.Error != nil {
+		c.logger.Errorf("exportExecEnvCfg. Error %v", *res.Error)
+		return "", "", errors.New(*res.Error)
+	}
+
+	c.logger.Debugf("exportExecEnvCfg. [%s] Exported %s", appliance_id, exec_env_name)
+
+	return *res.Config, *res.FileName, err
+
+}
+
 // Received an identity appliance name and starts it.
 func (c *IdbusApiClient) ActivateExecEnv(
 	appliance_id string,
